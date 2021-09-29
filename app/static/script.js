67049d76b4ef1550df608b6b52789c015bde90bb
lizzyjane340 <lizzyjane0121@protonmail.com>
2021-09-29 12:40:08 +0100

    Add client side input validation

--- app/static/script.js
+++ app/static/script.js
@@ -29,7 +29,7 @@ var months = ['January','February','March','April','May','June','July',
 
 var date = new Date();
 
-document.getElementById("spanDate").innerHTML = "session: " + date.toUTCString();
+document.getElementById('spanDate').innerHTML = "session: " + date.toUTCString();
 
 // file upload alert
 let uploadFile = document.getElementById('entry-file');
@@ -37,3 +37,74 @@ let uploadFile = document.getElementById('entry-file');
 uploadFile.addEventListener('change', () => {
     document.getElementById('uploadLabel').innerHTML = "File Uploaded";
 });
+
+// input length validation for forms
+const loginUsername = document.getElementById('loginUsername');
+
+loginUsername.addEventListener('input', () => {
+  if (loginUsername.validity.invalid) {
+    loginUsername.setCustomValidity = "Username should be 3 to 15 characters";
+  } else {
+    loginUsername.setCustomValidity("");
+  }
+});
+
+const loginPassword = document.getElementById('loginPassword');
+
+loginPassword.addEventListener('input', () => {
+    if (!loginPassword.checkValidity()) {
+      loginPassword.innerHTML = loginPassword.validationMessage;
+    } 
+  });
+
+  const usernameReg = document.getElementById('usernameReg');
+
+  usernameReg.addEventListener('input', () => {
+    if (usernameReg.checkValidity()) {
+      usernameReg.innerHTML = usernameReg.validationMessage;
+    } 
+  });
+
+  const passwordReg1 = document.getElementById('passwordReg1');
+  const passwordReg2 = document.getElementById('passwordReg2');
+
+  passwordReg1.addEventListener('input', () => {
+      if (passwordReg1.checkValidity()) {
+          passwordReg1.innerHTML = passwordReg1.validationMessage;
+      } 
+  });
+
+  passwordReg2.addEventListener('input', () => {
+    if (passwordReg2.checkValidity()) {
+        passwordReg2.innerHTML = passwordReg2.validationMessage;
+    } 
+});
+
+const entryTitle = document.getElementById('entryTitle');
+const entryLink = document.getElementById('entryLink');
+const entryDesc = document.getElementById('entryDesc');
+
+entryTitle.addEventListener('input', () => {
+    if (entryTitle.checkValidity()) {
+        entryTitle.innerHTML = entryTitle.validationMessage;
+    } 
+});
+
+entryLink.addEventListener('input', () => {
+    if (entryLink.checkValidity()) {
+        entryLink.innerHTML = entryLink.validationMessage;
+    } 
+});
+
+entryDesc.addEventListener('input', () => {
+    if (entryDesc.checkValidity()) {
+        entryDesc.innerHTML = entryDesc.validationMessage;
+    } 
+});
+
+//const catCheck = document.getElementById('catCheck');
+// TODO checkbox validation entry form
+//catCheck.addEventListener('change')
+
+
+
